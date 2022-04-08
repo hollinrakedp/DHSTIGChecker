11e8eced32ebb00060896548e4a3e9fd9f39d437
Darren Hollinrake <hollinrakedp@gmail.com>
2022-04-08 16:11:08 -0500

    Initial Commit: Get-STIGVulnInfo and STIG data

---
+++ DHSTIGChecker/Public/Get-STIGVulnInfo.ps1
@@ -0,0 +1,65 @@
+function Get-STIGVulnInfo {
+    [CmdletBinding(DefaultParameterSetName = 'Query')]
+    param (
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName)]
+        [ValidateScript({
+                if ($_ -in (& $TabCompleteAvailableSTIGs)) {
+                    $true
+                }
+                else {
+                    throw "The supplied value `'$_`' is invalid. Valid values: $($STIGPath.Name -join ', ')"
+                }
+            })]
+        [string]$Name,
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'Query',
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [string[]]$ID,
+        [Parameter(
+            ParameterSetName = 'ShowAll',
+            ValueFromPipelineByPropertyName)]
+        [switch]$All
+    )
+    
+    begin {
+        try {
+            $STIGInfo = Import-Csv -Path "$STIGRootPath\$Name\data.csv"
+            $defaultDisplaySet = 'Vuln ID', 'Severity', 'Rule Title'
+            $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet', [string[]]$defaultDisplaySet)
+            $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
+            $STIGInfo | Add-Member MemberSet PSStandardMembers $PSStandardMembers
+        }
+        catch {
+            Write-Warning "Unable to find the STIG vulnerability information file."
+            return
+        } 
+    }
+    
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            Query {
+                $Results = @()
+                foreach ($item in $ID) {
+                    $Results += $STIGInfo | Where-Object { ($_.'Vuln ID' -match $item) -or ($_.Legacy -match $item) }
+                }
+            }
+
+            All {
+                $Results = $STIGInfo
+            }
+        }
+        if ($Results.count -ge 1) {
+            $Results
+        }
+        else {
+            Write-Output "No match was found."
+        }
+    }
+    
+    end {
+    }
+}
